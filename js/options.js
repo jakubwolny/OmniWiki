b6ec314b425c94ef258c139ad38ae0c9d42c5482
Jakub Wolny <j.wolny@101studio.pl>
2012-11-26 11:53:00 +0100

    manifest version 2 - fixes #2

--- js/options.js
+++ js/options.js
@@ -1,49 +1,63 @@
 var data = {};
 try {
     data = JSON.parse(localStorage.options);   
-}catch(e){  
+} catch(e){  
 }
 var defaults = {
     lang: ''
 };
 
-var options = $.extend({}, defaults, data);
+var options = {};
+for(var id in defaults){
+    options[id] = data[id] === undefined ? defaults[id] : data[id];
+}
 
 window.onload = function(){
-   
-    $('.lang').text(function(){
-        //console.log($(this).attr('id') + ' => ' + chrome.i18n.getMessage($(this).attr('id')));
-        return chrome.i18n.getMessage($(this).attr('id'));
-    });
+    var save = document.getElementById('save');
+    var items = document.querySelectorAll('.lang');    
+    for(var i = 0; i < items.length; i++){
+        items[i].textContent = chrome.i18n.getMessage(items[i].id);
+    }
 
-    $('#save').val(chrome.i18n.getMessage('save'));
+    save.value = chrome.i18n.getMessage('save');
 
-    $('#restore_defaults').bind('click', function(){
+    document.getElementById('restore_defaults').addEventListener('click', function(){
         window.localStorage.options = JSON.stringify(defaults);
         location.reload();       
     });
 
     if(options.lang){
-        $('#lang').val(options.lang);
-    }else {
+        document.getElementById('lang').value = options.lang;
+    } else {
         chrome.i18n.getAcceptLanguages(function(languages) {
-            $('#lang').val(languages[0].substr(0, 2));
+            document.getElementById('lang').value = languages[0].substr(0, 2);
         });
     }
     
-    $('input[type="text"]').keyup(function(){
-        $('#save').removeClass('saved').val(chrome.i18n.getMessage('save'));
-    });
-
-    $('form').submit(function(){
+    document.getElementById('form').addEventListener('submit', function(e){
+        e.preventDefault();
         var data = {
-            lang: $('#lang').val()
+            lang: document.getElementById('lang').value
         }
         window.localStorage.options = JSON.stringify(data);
         chrome.extension.getBackgroundPage().updateOptions();
-        $('#save').addClass('saved').val(chrome.i18n.getMessage('saved'));
+        save.className = 'saved';
+        save.value = chrome.i18n.getMessage('saved');
         return false;
-    }).change(function(){
-        $('#save').removeClass('saved').val(chrome.i18n.getMessage('save'));
+    });
+    
+    document.getElementById('form').addEventListener('change', function(){
+        save.className = '';
+        save.value = chrome.i18n.getMessage('save');
     });
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7218577-47']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
